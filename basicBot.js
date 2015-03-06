9a5b7ea827a558273a8cef599ec4306bdd0dccbd
MCzeKrakan <mczekrakan@gmail.com>
2015-03-06 09:45:09 +0100

    stream and youtube update

--- basicBot.js
+++ basicBot.js
@@ -3014,8 +3014,22 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                        if (typeof basicBot.settings.czYoutubeLink === "string")
-                            API.sendChat(subChat(basicBot.chat.youtube, {name: chat.un, link: basicBot.settings.czYoutubeLink}));
+                        if (typeof basicBot.settings.czYoutubeLink === "string" %% typeof basicBot.settings.enYoutubeLink === "string")
+                            API.sendChat(subChat(basicBot.chat.youtube, {name: chat.un, linkcz: basicBot.settings.czYoutubeLink, linken: basicBot.settings.enYoutubeLink}));
+                    }
+                }
+            },
+            
+            streamCommand: {
+                command: 'stream',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        if (typeof basicBot.settings.streamLink === "string")
+                            API.sendChat(subChat(basicBot.chat.stream, {name: chat.un, link: basicBot.settings.streamLink}));
                     }
                 }
             }

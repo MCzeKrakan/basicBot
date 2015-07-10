0c2e8bb81b9920bcda6e4ae9d989488564f49ad7
MCzeKrakan <mczekrakan@gmail.com>
2015-07-10 15:35:13 +0200

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2318,7 +2318,7 @@
             
             historyskiplimitCommand: {
                 command: 'historyskiplimit',
-                rank: 'bouncer',
+                rank: 'manager',
                 type: 'startsWith',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
@@ -2342,7 +2342,7 @@
 
             historyskipCommand: {
                 command: 'historyskip',
-                rank: 'bouncer',
+                rank: 'mod',
                 type: 'exact',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);

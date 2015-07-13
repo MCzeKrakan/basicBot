5e870f105ed46fddfba1e4866f904859b615ec8b
MCzeKrakan <mczekrakan@gmail.com>
2015-07-13 16:48:18 +0200

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1830,7 +1830,7 @@
 
             bouncerPlusCommand: {
                 command: 'bouncer+',
-                rank: 'mod',
+                rank: 'manager',
                 type: 'exact',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);

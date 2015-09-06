b689b097755fea48e2ed46d265bc0945b88aed34
MCzeKrakan <mczekrakan@gmail.com>
2015-09-06 18:02:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
+    $('car').click(function() {
+        if ($(this).hasClass("selected")) {
+            $(this).removeClass("selected");
+        }
+        else {
+            $(this).addClass("selected");
+        }
+    });
     //$('#car').draggable();
 });

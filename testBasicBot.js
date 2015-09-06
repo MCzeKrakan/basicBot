81c7106d271c33a8640ed84c2aeeb316efb4f181
MCzeKrakan <mczekrakan@gmail.com>
2015-09-06 20:56:30 +0200

    Update testBasicBot.js

--- testBasicBot.js
+++ testBasicBot.js
@@ -57,7 +57,6 @@
             $.getScript('https://cdn.jsdelivr.net/sockjs/0.3.4/sockjs.min.js', loadSocket);
         } else loadSocket();
     }
-
     var sendToSocket = function () {
         var basicBotSettings = basicBot.settings;
         var basicBotRoom = basicBot.room;
@@ -1511,7 +1510,6 @@
                                 if(this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                                 if( !basicBot.commands.executable(this.rank, chat) ) return void (0);
                                 else{
-
                                 }
                         }
                 },
@@ -3739,14 +3737,12 @@
         }
     };
 
-    
+    loadChat(basicBot.startup);
+}).call(this);
+
     //$('head').append('<link rel="stylesheet" type="text/css" href="https://rawgit.com/MCzeKrakan/basicBot/master/basic.css"/>');
     //$('head').append('<script type="text/javascript" src="https://rawgit.com/MCzeKrakan/basicBot/master/script.js"></script>');
     //$('head').append('<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>');
     //var menuButton = $('<div id="car" class="header-panel-button" style=""><i class="icon" style="top: 16px; width: 42px; height: 25px; background: url(http://oi57.tinypic.com/2e2kyfq.jpg;"></i></div>');
     //var menuButton = $('<div id="car" class="header-panel-button" style=""><div id="top"></div><div id="bottom"></div><div id="fwheel"></div><div id="bwheel"></div></div>');
     //$('#friends-button').after(menuButton);
-    
-            
-    loadChat(basicBot.startup);
-}).call(this);

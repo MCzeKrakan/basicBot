7c7230dbe689bd681d61b4ec3d9b988bc1afaf88
MCzeKrakan <mczekrakan@gmail.com>
2015-09-07 22:04:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ $(document).ready(function() {
         if ($(this).hasClass("selected") === false) {
             $('.header-panel-button').removeClass("selected");
             $(this).addClass("selected");
-            $('.app-right').children.css('display', 'none');
+            $('#chat, #user-lists, #waitlist, .friends').css('display', 'none');
             $('#bsc-menu').css('display', 'block');
         }
     });
+    if ($(this).hasClass("selected") === false) {
+        $('#bsc-menu').css('display', 'none');
+    }
     //$('#car').draggable();
 });
